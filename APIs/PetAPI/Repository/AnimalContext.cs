13df631a7d3576be907884ae66e0641b088552a5
Ryan <rbeardshear@gmail.com>
2017-06-06 14:34:49 -0700

    adding APIs folder

---
+++ APIs/PetAPI/Repository/AnimalContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using PetAPI.Controllers;
+
+namespace PetAPI
+{
+  public class AnimalContext : DbContext
+  {
+    public AnimalContext(DbContextOptions<AnimalContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Animal> Animals { get; set; }
+  }
+
+
+}
