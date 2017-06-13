fbd31b09e8bed736bef220ab0243dd7da77538f7
Ryan <rbeardshear@gmail.com>
2017-06-13 16:49:18 -0700

    pulled

--- controllers/animalController.js
+++ controllers/animalController.js
@@ -1,12 +1,31 @@
-app.controller("animalController", function($scope, $http, animalService){
-    $scope.animals = [];
-    $scope.filterAll = true;
-    // uiGmapGoogleMapApi
-    
-    animalService.getAnimals().then(function(response){
-      $scope.animals = response.data;
-    })
+app.controller("animalController", function ($scope, $http, animalService) {
+  $scope.animals = [];
+  $scope.filterAll = true;
 
-    $scope.map = { center: { latitude: 33.703744, longitude: -117.846525 }, zoom: 15 };
-    
+  // uiGmapGoogleMapApi
+
+  animalService.getAnimals().then(function (response) {
+    $scope.animals = response.data;
+  })
+
+  $scope.map = { center: { latitude: 33.703744, longitude: -117.846525 }, zoom: 15 };
+
+  $scope.foundButton = function() {
+    $scope.filterFound = true;
+    $scope.filterLost = false;
+    $scope.filterAll = false;
+  }
+
+  $scope.lostButton = function() {
+    $scope.filterFound = false;
+    $scope.filterLost = true;
+    $scope.filterAll = false;
+   
+  }
+
+  $scope.allButton = function() {
+    $scope.filterFound = false;
+    $scope.filterLost = false;
+    $scope.filterAll = true;
+  }
 })
