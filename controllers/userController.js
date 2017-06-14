7ec61988b483307c1b4e9f85b36145279a2f303c
Ryan <rbeardshear@gmail.com>
2017-06-14 13:50:27 -0700

    made forms in User view appear one at a time, added buttons to other forms, made feilds required to advance to new post form, and added error messages to sign in page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,136 +1,177 @@
 angular
-.module("petApp")
-.controller("userController", function($scope, userService, animalService){
-var _username;
-var _password1;
-var _password2;
+  .module("petApp")
+  .controller("userController", function ($scope, userService, animalService) {
+    var _username;
+    var _password1;
+    var _password2;
 
-var SignedInUser;
+    var SignedInUser;
 
-var _name;
-var _type;
-var _breed;
-var _color;
-var _size;
-var _lostOrFound;
-var _date;
-$scope.Users = [];
-$scope.Animals = []
-var tempId = 0;
-var animalTempId = 0;
-var i = 0;
+    var _name;
+    var _type;
+    var _breed;
+    var _color;
+    var _size;
+    var _lostOrFound;
+    var _date;
+    $scope.Users = [];
+    $scope.Animals = []
+    var tempId = 0;
+    var animalTempId = 0;
+    var i = 0;
 
-userService.getUser().then(function(response){
-  $scope.Users = response.data;
-})
-animalService.getAnimals().then(function(response){
-  $scope.Animals = response.data;
-})
+    $scope.usernameSignIn = "";
+    $scope.passwordSignIn = "";
 
-// ________________________User form_____________________________
-$scope.UserSubmit = function(){
-  _username = $scope.username;
-  _password1 = $scope.password1;
-  _password2 = $scope.password2;
-  check = "";
-  if(_password1 != _password2){
-    console.log("Passwords don't match")
-  };
-  $scope.Users.forEach(function(user) {
-    if(_username == user.username){
-      console.log("Username taken")
-      i++;
-    }
-    if(_username != user.username){
-      console.log("Username doesn't match.")
-      tempId = user.id;
-    }
+    $scope.signInAcc = true;
+    $scope.createAcc = false;
+    $scope.createPost = false;
+    $scope.signInWarn = false;
+    $scope.wrongInfoWarn = false;
     
-  }, this);
-  if(i <= 0){
-    console.log(_username)
-    console.log(_password1)
-    console.log(_password2)
 
-    var user = {};
-    user.username = _username;
-    user.password = _password2;
-    user.id = tempId + 1;
-    userService.addUser(user);
-    userService.getUser().then(function(response){
-  $scope.Users = response.data;
-})
-    }
-    $scope.username = "";
-    $scope.password1 = "";
-    $scope.password2 = "";
-    i = 0;
-};
-
-
-// _______________________Sign In form___________________________
-$scope.SignIn = function(){
-  $scope.Users.forEach(function(User) {
-    if( ($scope.usernameSignIn == User.username) && ($scope.passwordSignin == User.password) ) {
-      SignedInUser = $scope.usernameSignIn;
-    }
-    else{
-      console.log("Username or password not Correct")
-    }
-  }, this);
-  console.log(SignedInUser);
-  $scope.usernameSignIn = "";
-  $scope.passwordSignin = "";
-}
-
-
-// _______________________Animal form____________________________
-
-$scope.animalSubmit = function(){
-  _name = $scope.name;
-  _type = $scope.type;
-  _breed = $scope.breed;
-  _color = $scope.color;
-  _size = $scope.size;
-  _lostOrFound = $scope.lostOrFound;
-
-  _address = $scope.address
-  _city = $scope.city
-  _state = $scope.state
-  _zip = $scope.zip
-
-  _date = $scope.date;
-  $scope.Animals.forEach(function(animal) {
-      animalTempId = animal.id;
-    }, this);
-
-    var animal = {};
-    animal.name = _name;
-    animal.type = _type;
-    animal.breed = _breed ;
-    animal.color = _color;
-    animal.size = _size;
-    animal.lostOrFound = _lostOrFound;
-    animal.location = _address + " " + _city + " " + _state + " " + _zip;
-    animal.date = _date;
-    animal.id = animalTempId + 1;
-    animal.user = SignedInUser
-    animalService.addAnimal(animal);
-    animalTempId = 0;
-    animalService.getAnimals().then(function(response){
+    userService.getUser().then(function (response) {
+      $scope.Users = response.data;
+    })
+    animalService.getAnimals().then(function (response) {
       $scope.Animals = response.data;
-})
-$scope.name = "";
-$scope.type = "";
-$scope.breed = "";
-$scope.color = "";
-$scope.size = "";
-$scope.lostOrFound = "";
-$scope.date = "";
-$scope.address = "";
-$scope.city = "";
-$scope.state = "";
-$scope.zip = "";
-}
-    
-});
+    })
+
+    // ________________________User form_____________________________
+    $scope.UserSubmit = function () {
+      _username = $scope.username;
+      _password1 = $scope.password1;
+      _password2 = $scope.password2;
+      check = "";
+      if (_password1 != _password2) {
+        console.log("Passwords don't match")
+      };
+      $scope.Users.forEach(function (user) {
+        if (_username == user.username) {
+          console.log("Username taken")
+          i++;
+        }
+        if (_username != user.username) {
+          console.log("Username doesn't match.")
+          tempId = user.id;
+        }
+
+      }, this);
+      if (i <= 0) {
+        console.log(_username)
+        console.log(_password1)
+        console.log(_password2)
+
+        var user = {};
+        user.username = _username;
+        user.password = _password2;
+        user.id = tempId + 1;
+        userService.addUser(user);
+        userService.getUser().then(function (response) {
+          $scope.Users = response.data;
+          $scope.createPost = true;
+          $scope.createAcc = false;
+          $scope.signInAcc = false;
+        })
+      }
+      $scope.username = "";
+      $scope.password1 = "";
+      $scope.password2 = "";
+      i = 0;
+    };
+
+
+    // _______________________Sign In form___________________________
+    $scope.SignIn = function () {
+      if (($scope.usernameSignIn == "") || ($scope.passwordSignIn == "")) {
+        $scope.signInWarn = true;
+      }
+      if (($scope.usernameSignIn != "") && ($scope.passwordSignIn != "")) {
+        $scope.signInWarn = false;
+      }
+      if(true){
+        $scope.Users.forEach(function (User) {
+          if (($scope.usernameSignIn == User.username) && ($scope.passwordSignIn == User.password)) {
+            SignedInUser = $scope.usernameSignIn;
+            $scope.createPost = true;
+            $scope.createAcc = false;
+            $scope.signInAcc = false;
+            console.log("Signed In")
+          }
+          else {
+            $scope.wrongInfoWarn = true;
+            console.log("Username or Password not Correct")
+          }
+        }, this);
+        console.log(SignedInUser);
+        $scope.usernameSignIn = "";
+        $scope.passwordSignin = "";
+
+      }
+    }
+
+
+    // _______________________Animal form____________________________
+
+    $scope.animalSubmit = function () {
+      _name = $scope.name;
+      _type = $scope.type;
+      _breed = $scope.breed;
+      _color = $scope.color;
+      _size = $scope.size;
+      _lostOrFound = $scope.lostOrFound;
+
+      _address = $scope.address
+      _city = $scope.city
+      _state = $scope.state
+      _zip = $scope.zip
+
+      _date = $scope.date;
+      $scope.Animals.forEach(function (animal) {
+        animalTempId = animal.id;
+      }, this);
+
+      var animal = {};
+      animal.name = _name;
+      animal.type = _type;
+      animal.breed = _breed;
+      animal.color = _color;
+      animal.size = _size;
+      animal.lostOrFound = _lostOrFound;
+      animal.location = _address + " " + _city + " " + _state + " " + _zip;
+      animal.date = _date;
+      animal.id = animalTempId + 1;
+      animal.user = SignedInUser
+      animalService.addAnimal(animal);
+      animalTempId = 0;
+      animalService.getAnimals().then(function (response) {
+        $scope.Animals = response.data;
+      })
+      $scope.name = "";
+      $scope.type = "";
+      $scope.breed = "";
+      $scope.color = "";
+      $scope.size = "";
+      $scope.lostOrFound = "";
+      $scope.date = "";
+      $scope.address = "";
+      $scope.city = "";
+      $scope.state = "";
+      $scope.zip = "";
+    }
+
+
+    $scope.SignInForm = function () {
+      $scope.createPost = false;
+      $scope.createAcc = false;
+      $scope.signInAcc = true;
+    }
+
+    $scope.CreateForm = function () {
+      $scope.createPost = false;
+      $scope.createAcc = true;
+      $scope.signInAcc = false;
+    }
+
+  });
+
